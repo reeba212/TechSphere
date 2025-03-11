f5f820accac1687ec54f5e2b7a3f6ca2070bf329
reeba212 <reebaq212@gmail.com>
2025-03-12 00:04:00 +0530

    create signup api route

---
+++ api/controllers/auth.controller.js
@@ -0,0 +1,20 @@
+import User from '../models/user.model.js';
+import bcryptjs from 'bcryptjs';
+
+export const signup = async (req, res) => {
+    const { username, email, password } = req.body;
+    
+    if (!username || !email || !password || username === '' || email === '' || password === '') {
+        return res.status(400).json({ message: 'All fields are required' });
+    }
+
+    try {
+        const hashedPassword = bcryptjs.hashSync(password, 10);
+        const newUser = new User({ username, email, password: hashedPassword });
+
+        await newUser.save();
+        res.json({ message: 'Signup successful' });
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
