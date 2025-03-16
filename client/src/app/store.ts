1c3743a7bc2efa2bd4b9fa6e7641bf4c8f33e0cd
reeba212 <reebaq212@gmail.com>
2025-03-16 20:42:16 +0530

    add redux persist

--- client/src/app/store.ts
+++ client/src/app/store.ts
@@ -1,13 +1,31 @@
-import { configureStore } from '@reduxjs/toolkit'
-import userReducer from '../features/userSlice'
+import { configureStore, combineReducers } from '@reduxjs/toolkit';
+import userReducer from '../features/userSlice';
+import persistReducer from 'redux-persist/es/persistReducer';
+import storage from 'redux-persist/lib/storage';
+import persistStore from 'redux-persist/es/persistStore';
+
+// Corrected combineReducers syntax
+const rootReducer = combineReducers({
+  user: userReducer,
+});
+
+// Corrected persistConfig key
+const persistConfig = {
+  key: 'root',
+  storage,
+  version: 3,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-  reducer: {
-    user: userReducer,
-  },
-})
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({ serializableCheck: false }),
+});
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
-export type RootState = ReturnType<typeof store.getState>
-// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
-export type AppDispatch = typeof store.dispatch
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
+
+export const persistor = persistStore(store);
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,11 +3,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { store } from './app/store'
+import { store, persistor } from './app/store'
 import { Provider } from 'react-redux'
+import { PersistGate } from 'redux-persist/integration/react';
 
 createRoot(document.getElementById('root')).render(
-  <Provider store={store}>
+  <PersistGate persistor={persistor}>
+    <Provider store={store}>
       <App />
-  </Provider>,
+    </Provider>,
+  </PersistGate>
 )
